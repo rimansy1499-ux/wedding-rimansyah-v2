3c5855e32a6642a85dad31a02c6db56b8b63099a
rimansy1499-ux <rimansy1499@gmail.com>
2026-07-08 02:55:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
         });
     }
+    const targetDate = new Date("September 6, 2026 08:00:00").getTime();
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+
+    const minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+
+    const seconds = Math.floor((distance % (1000*60))/1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+},1000);
 
 });
